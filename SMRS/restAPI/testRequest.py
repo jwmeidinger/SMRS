2239f5df810b109b804d3a268f567c700303adce
jwmeidinger <jwmeidinger99@hotmail.com>
2020-02-21 15:15:59 -0600

    comments

--- SMRS/restAPI/testRequest.py
+++ SMRS/restAPI/testRequest.py
@@ -25,14 +25,14 @@ head = {'Authorization': 'Token {}'.format(myToken)}
 """
 *** This is an example of creating a new Team
 """
-data = {"name": "D - Team"}
-r = requests.post('http://127.0.0.1:8000/restAPI/Team/', headers = head, data=data)
-print(r.text)
+#data = {"name": "D - Team"}
+#r = requests.post('http://127.0.0.1:8000/restAPI/Team/', headers = head, data=data)
+#print(r.text)
 
 """
 *** This is an example of Deleting a Team
 """
-item = '6' ## remember to change the ID of team you want to change
-r = requests.delete('http://127.0.0.1:8000/restAPI/Team/{}'.format(item),headers = head)
-print(r.text)
+#item = '6' ## remember to change the ID of team you want to change
+#r = requests.delete('http://127.0.0.1:8000/restAPI/Team/{}'.format(item),headers = head)
+#print(r.text)
 
