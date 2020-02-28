23c8dcafd7b412e62d747cd6d3dd99c3ee14d949
sirdude103 <sirdude103@gmail.com>
2020-02-28 00:45:50 -0600

    Improved tables

--- SMRS/dashboard/views.py
+++ SMRS/dashboard/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from plotly.offline import plot
 import plotly.graph_objs as graph_objs
+import random
 
 from restAPI.models import Project, Review, Defect, ProjectNumber, PhaseType
 from account.models import Team, Account
@@ -19,30 +20,66 @@ def home_view(request):
     phase_type_values = [val['phase_type'] for val in list(phase_type.values('phase_type'))]
 
     defects = Defect.objects.all()
-    defect_values = defects.values("whereFound", "id")
+    projects = Project.objects.all()
+    reviews = Review.objects.all()
+    project_values = projects.values("name", "id")
+    defect_values = defects.values("whereFound", "id", "projectID")
+    review_values = reviews.values("dateOpened")
+
+    colors = ["red", "green", "blue", "orange"]
+    fig = graph_objs.Figure()
+
+    for proj_val in project_values:
+        name = proj_val["name"]
+
+        counts = dict()
+        for i in range(len(phase_type_values)):
+            counts[i] = 0
+
+        for defect in defect_values:
+            if defect["projectID"] == proj_val["id"]:
+                counts[defect['whereFound']-1] += 1
+
+        if colors:
+            my_color = random.choice(colors)
+            colors.remove(my_color)
+        else:
+            my_color = "black"
+
+        new_scatter = graph_objs.Bar(x=phase_type_values, y=list(counts.values()),
+            name=name,
+            opacity=0.8, marker_color=my_color
+        )
+        fig.add_trace(new_scatter)
+
+
+        new_scatter = graph_objs.Scatter(x=phase_type_values, y=list(counts.values()))
+
+    fig.update_layout(title="Defects Where Found",
+                    xaxis_title="Phases",
+                    yaxis_title="Defects")
+
+    fig2 = graph_objs.Figure()
 
     counts = dict()
-    for i in range(len(phase_type_values)):
+    for i in range(1, 13):
         counts[i] = 0
 
-    for defect in defect_values:
-        counts[defect['whereFound']-1] += 1
+    for review in review_values:
+        review_month = review["dateOpened"].month
+        counts[review_month] += 1
+    for i in range(1, 13):
+        if not counts[i]:
+            del counts[i]
 
-    #defect_values = [val['phase_type'] for val in list(defects.values('phase_type'))]
-    #print(defect_values)
-
-    fig = graph_objs.Figure()
-    scatter = graph_objs.Bar(x=phase_type_values, y=list(counts.values()),
-                        name='John Deere Graphs',
-                        opacity=0.8, marker_color='green')
-    scatter2 = graph_objs.Bar(x=phase_type_values, y=list(counts.values()),
-                    name='John Deere Graphs',
-                    opacity=0.8, marker_color='red')
-    fig.add_trace(scatter)
-    fig.add_trace(scatter2)
-    plt_div = plot(fig, output_type='div', include_plotlyjs=False, show_link=False, link_text="")
-    context['graph'] = plt_div
-    context['graph2'] = plot(fig, output_type='div', include_plotlyjs=False, show_link=False, link_text="")
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    new_scatter = graph_objs.Scatter(x=months, y=list(counts.values()))
+    fig2.add_trace(new_scatter)
+    fig2.update_layout(title="Reviews over time",
+                    xaxis_title="Months",
+                    yaxis_title="Reviews")
+    context['graph'] = plot(fig, output_type='div', include_plotlyjs=False, show_link=False, link_text="")
+    context['graph2'] = plot(fig2, output_type='div', include_plotlyjs=False, show_link=False, link_text="")
     
     user = request.user
     if user.is_authenticated:
