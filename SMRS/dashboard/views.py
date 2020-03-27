77c6c1e77f25847fd0bdc8e24158633b749bb14c
sirdude103 <sirdude103@gmail.com>
2020-03-26 20:21:51 -0500

    Resolved merge conflicts

--- SMRS/dashboard/views.py
+++ SMRS/dashboard/views.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from rest_framework import viewsets, permissions
 from plotly.offline import plot
 import plotly.graph_objs as graph_objs
@@ -60,7 +61,6 @@ def home_view(request):
 '''
 *** User's Team, members, and Projects
 '''
-## TODO: Need to Finish CSS
 def team_view(request):
     context= {}
     user = request.user
@@ -76,13 +76,12 @@ def team_view(request):
 '''
 *** User's Personally selected Projects
 '''
-## TODO: Need to Finish CSS
 def project_view(request):
     context= {}
     user = request.user
     if user.is_authenticated:
         value = request.COOKIES.get('FavoriteProjects')
-        if value != None: ## Browers never on the page before
+        if value != None: ## Browser never on the page before
             favs = set(value.split(","))
             blankspace  = ",".join(favs)
             if blankspace != "": ## if empty don't make a request to DB
@@ -99,18 +98,36 @@ def project_view(request):
 '''
 *** User's Personally selected Projects
 '''
-## TODO: Need to Finish HTML and CSS
+## TODO: Need to add Graph and Table
 def projectDetail_view(request, pk):
     context= {}
     user = request.user
-    if user.is_authenticated:
-        project_detail = get_object_or_404(Project, pk=pk)
-        allDefects = Defect.objects.filter(projectID=project_detail)
-        allReviews = Review.objects.filter(projectID=project_detail)
+    
+    project_detail = get_object_or_404(Project, pk=pk) # Get the project info
+    allDefects = Defect.objects.filter(projectID=project_detail) # Get the Defects of project
+    allReviews = Review.objects.filter(projectID=project_detail) # Get the Reviews of project
 
-        context['projectName'] = project_detail.name
-        context['allDefects'] = allDefects
-        context['allReviews'] = allReviews
+    ## This allows to have pages of items 
+    paginator = Paginator(allDefects, 15) ## To switch items per page change the number
+    try: # If no items
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+    try:# If page is not found
+        allDefects = paginator.page(page)
+    except:
+        allDefects = paginator.page(paginator.num_pages)
+
+    ## Used to filter open Defects
+    if request.POST:
+        if request.POST["defectCheck"] != "0":
+            openDefects = Defect.objects.filter(projectID=project_detail, dateClosed__isnull=True)
+            context['openDefects'] = openDefects
+
+    ## Setting items to the template
+    context['allDefects'] = allDefects
+    context['allReviews'] = allReviews
+    context['projectName'] = project_detail.name
     return render(request, "dashboard/project_detail.html", context)
 
 '''
@@ -119,7 +136,7 @@ def projectDetail_view(request, pk):
 def projectAddFav_view(request, pk):
     value = request.COOKIES.get('FavoriteProjects')
     projectpk = pk ## have to declare before response
-    response = redirect("dashboard:project")
+    response = redirect("dashboard:project_team")
     if value == None or value == "": ## No Favorite set yet
         response.set_cookie('FavoriteProjects', '{}'.format(projectpk))
     else: ## Have Favorite
@@ -144,23 +161,60 @@ def projectDelFav_view(request, pk):
     projectpk = pk
     items = set(value.split(','))
     items.remove("{}".format(projectpk))
-    response = redirect("dashboard:project")
+    response = redirect("dashboard:project_team")
     final  = ",".join(items)
     response.set_cookie('FavoriteProjects', final)
     return response
 
+'''
+*** All Projects viewable by everyone
+'''
+def projectAll_view(request):
+    context= {}
+    projects = Project.objects.all()
+
+    ## This allows to have pages of items 
+    paginator = Paginator(projects, 20) ## To switch items per page change the number
+    try: # If no items
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+    try:# If page is not found
+        projects = paginator.page(page)
+    except:
+        projects = paginator.page(paginator.num_pages)
+
+    context["projects"] = projects
+    
+    if request.POST:
+        filterItem = request.POST.get('filterItem')
+        filterInput = request.POST.get('filterInput')
+        if filterItem == "projectName":
+            projects = Project.objects.filter(name=filterInput).all()
+        if filterItem == "projectTeam":
+            teamID = Team.objects.filter(name=filterInput).first()
+            projects = Project.objects.filter(teamID=teamID).all()
+        if filterItem == "projectOwner":
+            productOwner = Account.objects.filter(name=filterInput).first()
+            projects = Project.objects.filter(productOwner=productOwner).all()
+        if filterItem == "projectPk":
+            projects = Project.objects.filter(pk=int(filterInput)).all() 
+
+    context["projects"] = projects
+    return render(request, "dashboard/project_all.html", context)
 
 '''
 *** User's Personally selected Projects
 '''
-## TODO: Need  ONLY Team Leader to be able to change items
 def defect_view(request, pk):
     context= {}
     user = request.user
     if not user.is_authenticated:
-        return redirect("acccount:login")
+        return redirect("account:login")
     
     currentDefect = Defect.objects.filter(pk = pk).first()
+    project = Project.objects.filter(pk = currentDefect.projectID.pk).first()
+
     if request.POST:
         form = DefectForm(request.POST, instance = currentDefect)
         if form.is_valid():
@@ -173,8 +227,11 @@ def defect_view(request, pk):
                 "severity"  : request.POST["severity"],
                 "url"       : request.POST["url"]
             }
-            form.save()
-            context["success_message"] = "Updated"
+            if project.productOwner == user or user.is_admin:
+                form.save()
+                context["success_message"] = "Updated"
+            else:
+                context["success_message"] = "Must be Product Owner or Admin"
     else:
         form = DefectForm(
              initial = {
@@ -193,14 +250,14 @@ def defect_view(request, pk):
 '''
 *** User's Personally selected Projects
 '''
-## TODO: Need ONLY Team Leader to be able to change items
 def review_view(request, pk):
     context= {}
     user = request.user
     if not user.is_authenticated:
-        return redirect("acccount:login")
+        return redirect("account:login")
     
     currentReview = Review.objects.filter(pk = pk).first()
+    project = Project.objects.filter(pk = currentReview.projectID.pk).first()
 
     if request.POST:
         form = ReviewForm(request.POST)
@@ -214,8 +271,11 @@ def review_view(request, pk):
                 "severity"  : request.POST["severity"],
                 "url"       : request.POST["url"]
             }
-            form.save()
-            context["success_message"] = "Updated"
+            if project.productOwner == user or user.is_admin:
+                form.save()
+                context["success_message"] = "Updated"
+            else:
+                context["success_message"] = "Must be Product Owner or Admin"
     else:
         form = ReviewForm(
              initial = {
@@ -230,3 +290,11 @@ def review_view(request, pk):
         )
     context['form'] = form
     return render(request, "dashboard/review_detail.html", context)
+
+'''
+*** Information on website
+'''
+def about_view (request):
+    context= {}
+
+    return render(request, "dashboard/about.html", context)
